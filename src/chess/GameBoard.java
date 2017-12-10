3eb01c9125e8d4816761a791060540b658abdcfa
Kyrillos Nagy <kyrillos.n.wadie@gmail.com>
2017-12-10 18:45:14 +0200

    Adding Backgorund

--- src/chess/GameBoard.java
+++ src/chess/GameBoard.java
@@ -5,17 +5,24 @@
  */
 package chess;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author Kero
  */
 public class GameBoard extends javax.swing.JFrame {
+    ImageIcon z1 = new ImageIcon(getClass().getResource("imgs/background.png"));
 
     /**
      * Creates new form GameBoard
      */
     public GameBoard() {
         initComponents();
+        BackGroundLbl.setIcon(z1);
+        
     }
 
     /**
@@ -28,18 +35,21 @@ public class GameBoard extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        BackGroundLbl = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        BackGroundLbl.setText("jLabel1");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 648, Short.MAX_VALUE)
+            .addComponent(BackGroundLbl, javax.swing.GroupLayout.DEFAULT_SIZE, 648, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 410, Short.MAX_VALUE)
+            .addComponent(BackGroundLbl, javax.swing.GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -87,11 +97,14 @@ public class GameBoard extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new GameBoard().setVisible(true);
+                
             }
         });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BackGroundLbl;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
