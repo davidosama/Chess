a72bcd219221956fdb5b96e06427efa4b2347adf
David Osama <david_u_riad@hotmail.com>
2017-12-16 20:17:41 +0200

    finished Pawn except for switchPawn

--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -10,21 +10,26 @@ public class Pawn extends Piece{
 
     @Override
     public void move(int x, int y) {
-        //check if it's the king at that position
         if (validateMove(x, y)){
-            this.position.setLocation(x, y);
-            if(y == 0 || y == 7){
-                switchPawn(x, y);
+            if (GameBoard.isKing(x, y)){
+                GameBoard.Checkmate(this.color);
+            }
+            else{
+               this.position.setLocation(x, y);
+                if(y == 0 || y == 7){
+                    switchPawn(x, y);
+                }
+                if(!GameBoard.isEmpty(x, y)){
+                    GameBoard.attack(x, y);
+                }
             }
         }
-        //add fucntion attack(abstract Piece) to check if the new position eats the opponent position
     }
 
     @Override
     public boolean validateMove(int x, int y) {
         if (this.color.equals("Black")){//black sets are above so the Pawn is allowed to move down only
-            // a missing function here implemented in gameboard that checks if there is a piece in (x,y) if true returns that object
-            if(false){//if there is no piece and the return of the previous function is null/false
+            if (GameBoard.isEmpty(x,y)){//if there is no piece and the return of the previous function is null/false
                 if( x == this.position.getX() && y == this.position.getY()+1){
                     return true;
                 }
@@ -32,15 +37,14 @@ public class Pawn extends Piece{
                     return true;
                 }
             }
-            else if(true){//if there is a piece and it's the of the opposite color so it's attack that piece
+            else if (GameBoard.isEnemy(x, y, this.color)){//if there is a piece and it's the of the opposite color so it's attacking that piece
                 if( (x == this.position.getX()-1 || x == this.position.getX()+1) && y == this.position.getY()+1){
                     return true;
                 }  
             }
         }
         else if(this.color.equals("White")){//white sets are below so the Pawn is allowed to move up only
-            // a missing function here implemented in gameboard that checks if there is a piece in (x,y) if true returns that object
-            if(false){//if there is no piece and the return of the previous function is null/false
+            if(GameBoard.isEmpty(x,y)){//if there is no piece and the return of the previous function is null/false
                 if( x == this.position.getX() && y == this.position.getY()-1){
                     return true;
                 }
@@ -48,7 +52,7 @@ public class Pawn extends Piece{
                     return true;
                 }
             }
-            else if(false){//if there is a piece and it's the of the opposite color so it's attack that piece
+            else if(GameBoard.isEnemy(x, y, this.color)){//if there is a piece and it's the of the opposite color so it's attacking that piece
                 if( (x == this.position.getX()-1 || x == this.position.getX()+1) && y == this.position.getY()-1){
                     return true;
                 }  
@@ -57,7 +61,7 @@ public class Pawn extends Piece{
         return false;
     }
     
-    public void switchPawn(int x, int y){// swich the pawn with a dead piece when it reaches the end
+    public void switchPawn(int x, int y){// swich the pawn with a dead piece when it reaches the end. should be placed in GameBoard??
         
     }
 }
