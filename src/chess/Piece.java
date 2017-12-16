c1aaef8eb2772c5d3fb406b64b7e62d235c6280d
MinaWagdi <mina.wagdi63@gmail.com>
2017-12-16 20:07:54 +0200

    Revert "Revert "initialize pieces""
    This reverts commit 0f1a37c1564a6f1a9bb43730a84cca8d1ed272f7.

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,24 +1,37 @@
 package chess;
 
 
+import java.awt.Image;
 import java.awt.Point;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public abstract class Piece {
     int numOfMoves;
     String color;
     Point position;
     boolean alive;
-    ImageIcon img;
+    JLabel label;
     String pieceType;
     
-    public Piece(String color, int positionX, int positionY, ImageIcon img, String pieceType){
+    public Piece(String color, Point position){
         this.numOfMoves = 0;
         this.color = color;
-        this.position = new Point(positionX, positionY);
+        this.position = position;
         this.alive = true;
-        this.img = img;
+        loadImage();
         this.pieceType = pieceType;
+    }
+    
+    public void loadImage()
+    {
+        String imageName = this.getClass().getName();
+        imageName = imageName.replace("chess.", "");
+        imageName=color+imageName;
+        System.out.println("Image Name is : "+imageName);
+        ImageIcon i = new ImageIcon (getClass().getResource("/chess/imgs/"+imageName+".png")) ; 
+        label=new JLabel(i);
+        //label.setSize(60, 60);
     }
     
     public abstract void move(int x, int y);
