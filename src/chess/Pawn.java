fcea99d2c83659070f26a39be442aa17f47490b7
MinaWagdi <MinaWagdi@users.noreply.github.com>
2017-12-22 10:48:42 +0200

    Revert "Full Of bugs here"
    This reverts commit dbb054702218bad57fd14a5726c5364a4900f541.

--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -4,108 +4,105 @@ import java.awt.Point;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Pawn extends Piece implements Cloneable, Serializable {
+public class Pawn extends Piece implements Cloneable, Serializable{
 
     public Pawn(String color, Point position) {
         super(color, position);
     }
-
-    public Pawn(String color, Point position, boolean alive, String pieceType, int numOfMoves) {
+    
+    public Pawn(String color, Point position, boolean alive, String pieceType, int numOfMoves){
         super(color, position, alive, pieceType, numOfMoves);
     }
-
+    
     @Override
     public boolean move(int x, int y) {
         if (validateMove(x, y)) {
-            int OldX = (int) this.position.getX();
-            int OldY = (int) this.position.getY();
+            if (GameBoard.isKing(x, y, this.color)) {
+                GameBoard.Checkmate(this.color);
+            } else {
+                if (y == 0 || y == 7) {
+                    switchPawn(x, y);
+                }
+                if (!GameBoard.isEmpty(x, y)) {
+                    GameBoard.attack(x, y);
+                }
+            }
             this.position.setLocation(x, y);
-            if (this.color.equalsIgnoreCase("Black") && GameBoard.isTileThreatened("Black", (int) King.BlackKingPosition.getX(), (int) King.BlackKingPosition.getY())) {
-                //undo the setLocation 
-                this.position.setLocation(OldX, OldY);
-                return false;
-            } else if (this.color.equalsIgnoreCase("White") && GameBoard.isTileThreatened("White", (int) King.WhiteKingPosition.getX(), (int) King.WhiteKingPosition.getY())) {
-                this.position.setLocation(OldX, OldY);
-                return false;
-            }
-            if (y == 0 || y == 7) {
-                switchPawn(x, y);
-            }
-            if (!GameBoard.isEmpty(x, y)) {
-                GameBoard.attack(x, y);
-            }
             this.numOfMoves++;
+            
             return true;
         }
         return false;
     }
 
     @Override
-    public boolean moveAI(int x, int y, ArrayList<Piece> AllPieces) {
-        if (validateMoveAI(x, y, AllPieces)) {
-            int OldX = (int) this.position.getX();
-            int OldY = (int) this.position.getY();
+    public boolean moveAI(int x, int y, ArrayList<Piece> AllPiece) {
+        if (validateMoveAI(x,y,AllPiece)) {
+            if (isKingAI(x, y, this.color, AllPiece)) {
+                CheckmateAI(this.color, AllPiece);
+            }
+            else {
+                if (y == 0 || y == 7) {
+                    switchPawn(x, y);
+                }
+                if (isEmptyAI(x, y, AllPiece)) {
+                    attackAI(x, y, AllPiece);
+                }
+            }
             this.position.setLocation(x, y);
-            if (this.color.equalsIgnoreCase("Black") && GameBoard.isTileThreatenedAI("Black", (int) King.BlackKingPosition.getX(), (int) King.BlackKingPosition.getY(), AllPieces)) {
-                //undo the setLocation 
-                this.position.setLocation(OldX, OldY);
-                return false;
-            } else if (this.color.equalsIgnoreCase("White") && GameBoard.isTileThreatenedAI("White", (int) King.WhiteKingPosition.getX(), (int) King.WhiteKingPosition.getY(), AllPieces)) {
-                this.position.setLocation(OldX, OldY);
-                return false;
-            }
-            if (y == 0 || y == 7) {
-                switchPawn(x, y);
-            }
-            if (!isEmptyAI(x, y, AllPieces)) {
-                attackAI(x, y, AllPieces);
-            }
             this.numOfMoves++;
+            
             return true;
         }
         return false;
     }
-
+    
     public boolean canAttack(int x, int y) {
         if (this.color.equals("Black")) {
             if ((x == this.position.getX() - 1 || x == this.position.getX() + 1) && y == this.position.getY() - 1) {
                 return true;
             }
             return false;
-        } else {
-            if ((x == this.position.getX() - 1 || x == this.position.getX() + 1) && y == this.position.getY() + 1) {
-                return true;
-            }
-            return false;
+        } 
+        else{
+             if ((x == this.position.getX() - 1 || x == this.position.getX() + 1) && y == this.position.getY() + 1) {
+                    return true;
+                }
+             return false;
         }
-
+        
     }
 
     @Override
     public boolean validateMove(int x, int y) {
-        if (this.color.equals("Black")) {//black sets are above so the Pawn is allowed to move down only
-            if (GameBoard.isEmpty(x, y)) {//if there is no piece and the return of the previous function is null/false
-                if (x == this.position.getX() && y == this.position.getY() - 1) {
-                    return true;
-                } else if (this.numOfMoves == 0 && x == this.position.getX() && y == this.position.getY() - 2) {
+        if (this.color.equals("Black")){//black sets are above so the Pawn is allowed to move down only
+            if (GameBoard.isEmpty(x,y)){//if there is no piece and the return of the previous function is null/false
+                if( x == this.position.getX() && y == this.position.getY()-1){
                     return true;
                 }
-            } else if (GameBoard.isEnemy(x, y, this.color)) {//if there is a piece and it's the of the opposite color so it's attacking that piece
-                if ((x == this.position.getX() - 1 || x == this.position.getX() + 1) && y == this.position.getY() - 1) {
+                else if( this.numOfMoves == 0 && x == this.position.getX() && y == this.position.getY()-2){
                     return true;
                 }
             }
-        } else if (this.color.equals("White")) {//white sets are below so the Pawn is allowed to move up only
-            if (GameBoard.isEmpty(x, y)) {//if there is no piece and the return of the previous function is null/false
-                if (x == this.position.getX() && y == this.position.getY() + 1) {
+            else if (GameBoard.isEnemy(x, y, this.color)){//if there is a piece and it's the of the opposite color so it's attacking that piece
+                if( (x == this.position.getX()-1 || x == this.position.getX()+1) && y == this.position.getY()-1){
                     return true;
-                } else if (this.numOfMoves == 0 && x == this.position.getX() && y == this.position.getY() + 2) {
+                }  
+            }
+        }
+        else if(this.color.equals("White")){//white sets are below so the Pawn is allowed to move up only
+            if(GameBoard.isEmpty(x,y)){//if there is no piece and the return of the previous function is null/false
+                if( x == this.position.getX() && y == this.position.getY()+1){
                     return true;
                 }
-            } else if (GameBoard.isEnemy(x, y, this.color)) {//if there is a piece and it's the of the opposite color so it's attacking that piece
-                if ((x == this.position.getX() - 1 || x == this.position.getX() + 1) && y == this.position.getY() + 1) {
+                else if( this.numOfMoves == 0  &&  x == this.position.getX() && y == this.position.getY()+2){
                     return true;
                 }
+            }
+            else if(GameBoard.isEnemy(x, y, this.color)){//if there is a piece and it's the of the opposite color so it's attacking that piece
+                if( (x == this.position.getX()-1 || x == this.position.getX()+1) && y == this.position.getY()+1){
+                    return true;
+                }  
             }
         }
         return false;
@@ -113,49 +110,56 @@ public class Pawn extends Piece implements Cloneable, Serializable {
 
     @Override
     public boolean validateMoveAI(int x, int y, ArrayList<Piece> AllPieces) {
-        if (x > 7 || x < 0 || y > 7 || y < 0) {
+        if( x>7 || x <0 || y>7 || y<0){
             return false;
         }
-        if (this.color.equals("Black")) {//black sets are above so the Pawn is allowed to move down only
-            if (isEmptyAI(x, y, AllPieces)) {//if there is no piece and the return of the previous function is null/false
-                if (x == this.position.getX() && y == this.position.getY() - 1) {
-                    return true;
-                } else if (this.numOfMoves == 0 && x == this.position.getX() && y == this.position.getY() - 2) {
+        if (this.color.equals("Black")){//black sets are above so the Pawn is allowed to move down only
+            if (isEmptyAI(x,y,AllPieces)){//if there is no piece and the return of the previous function is null/false
+                if( x == this.position.getX() && y == this.position.getY()-1){
                     return true;
                 }
-            } else if (isEnemyAI(x, y, this.color, AllPieces)) {//if there is a piece and it's the of the opposite color so it's attacking that piece
-                if ((x == this.position.getX() - 1 || x == this.position.getX() + 1) && y == this.position.getY() - 1) {
+                else if( this.numOfMoves == 0 && x == this.position.getX() && y == this.position.getY()-2){
                     return true;
                 }
             }
-        } else if (this.color.equals("White")) {//white sets are below so the Pawn is allowed to move up only
-            if (isEmptyAI(x, y, AllPieces)) {//if there is no piece and the return of the previous function is null/false
-                if (x == this.position.getX() && y == this.position.getY() + 1) {
+            else if (isEnemyAI(x, y, this.color,AllPieces)){//if there is a piece and it's the of the opposite color so it's attacking that piece
+                if( (x == this.position.getX()-1 || x == this.position.getX()+1) && y == this.position.getY()-1){
                     return true;
-                } else if (this.numOfMoves == 0 && x == this.position.getX() && y == this.position.getY() + 2) {
+                }  
+            }
+        }
+        else if(this.color.equals("White")){//white sets are below so the Pawn is allowed to move up only
+            if(isEmptyAI(x,y,AllPieces)){//if there is no piece and the return of the previous function is null/false
+                if( x == this.position.getX() && y == this.position.getY()+1){
                     return true;
                 }
-            } else if (isEnemyAI(x, y, this.color, AllPieces)) {//if there is a piece and it's the of the opposite color so it's attacking that piece
-                if ((x == this.position.getX() - 1 || x == this.position.getX() + 1) && y == this.position.getY() + 1) {
+                else if( this.numOfMoves == 0  &&  x == this.position.getX() && y == this.position.getY()+2){
                     return true;
                 }
+            }
+            else if(isEnemyAI(x, y, this.color,AllPieces)){//if there is a piece and it's the of the opposite color so it's attacking that piece
+                if( (x == this.position.getX()-1 || x == this.position.getX()+1) && y == this.position.getY()+1){
+                    return true;
+                }  
             }
         }
         return false;
     }
-
+    
     public void switchPawn(int x, int y) {// swich the pawn with a dead piece when it reaches the end. should be placed in GameBoard??
 
     }
 
     @Override
     protected Pawn clone() throws CloneNotSupportedException {
-        Pawn k = new Pawn(this.color, new Point((int) this.position.getX(), (int) this.position.getY()));
+        Pawn k = new Pawn(this.color, new Point((int)this.position.getX(), (int)this.position.getY()));
         k.alive = this.alive;
         k.numOfMoves = this.numOfMoves;
         k.pieceType = this.pieceType;
         k.label = null;
-        return k;
+       return k;
     }
-
+    
+    
+    
 }
--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -22,20 +22,15 @@ public class Queen extends Piece implements Cloneable,Serializable{
 
     @Override
     public boolean move(int x, int y) {
-        if (validateMove(x, y)) {
-            int OldX = (int) this.position.getX();
-            int OldY = (int) this.position.getY();
-            this.position.setLocation(x, y);
-            if (this.color.equalsIgnoreCase("Black") && GameBoard.isTileThreatened("Black", (int) King.BlackKingPosition.getX(), (int) King.BlackKingPosition.getY())) {
-                //undo the setLocation 
-                this.position.setLocation(OldX, OldY);
-                return false;
-            } else if (this.color.equalsIgnoreCase("White") && GameBoard.isTileThreatened("White", (int) King.WhiteKingPosition.getX(), (int) King.WhiteKingPosition.getY())) {
-                this.position.setLocation(OldX, OldY);
-                return false;
+        if (validateMove(x, y)){
+            if (GameBoard.isKing(x, y,this.color)){
+                GameBoard.Checkmate(this.color);
             }
-            if (!GameBoard.isEmpty(x, y)) {
-                GameBoard.attack(x, y);
+            else{
+               if(!GameBoard.isEmpty(x, y)){
+                    GameBoard.attack(x, y);
+                }
+               this.position.setLocation(x, y);
             }
             return true;
         }
@@ -104,20 +99,15 @@ public class Queen extends Piece implements Cloneable,Serializable{
 
     @Override
     public boolean moveAI(int x, int y, ArrayList<Piece> AllPieces) {
-        if (validateMoveAI(x, y, AllPieces)) {
-            int OldX = (int) this.position.getX();
-            int OldY = (int) this.position.getY();
-            this.position.setLocation(x, y);
-            if (this.color.equalsIgnoreCase("Black") && GameBoard.isTileThreatenedAI("Black", (int) King.BlackKingPosition.getX(), (int) King.BlackKingPosition.getY(), AllPieces)) {
-                //undo the setLocation 
-                this.position.setLocation(OldX, OldY);
-                return false;
-            } else if (this.color.equalsIgnoreCase("White") && GameBoard.isTileThreatenedAI("White", (int) King.WhiteKingPosition.getX(), (int) King.WhiteKingPosition.getY(), AllPieces)) {
-                this.position.setLocation(OldX, OldY);
-                return false;
+        if (validateMoveAI(x, y, AllPieces)){
+            if (isKingAI(x, y,this.color,AllPieces)){
+                CheckmateAI(this.color,AllPieces);
             }
-            if (!isEmptyAI(x, y, AllPieces)) {
-                attackAI(x, y, AllPieces);
+            else{
+               if(!isEmptyAI(x, y,AllPieces)){
+                    attackAI(x, y,AllPieces);
+                }
+               this.position.setLocation(x, y);
             }
             return true;
         }
