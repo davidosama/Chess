7f5077385f9c60186c9a49dc75993966074c329e
MinaWagdi <MinaWagdi@users.noreply.github.com>
2017-12-22 11:40:05 +0200

    s

--- src/chess/GameBoard.java
+++ src/chess/GameBoard.java
@@ -1007,6 +1007,9 @@ public class GameBoard extends javax.swing.JFrame implements MouseListener ,Seri
             }
         }
         setPosions();
+        if(checkCheckmate(AllPiecesCloned).equals("Black")||checkCheckmate(AllPiecesCloned).equals("Black")){
+            JOptionPane.showMessageDialog(null, "CHECKAMTE");
+        }
     }
     
     public static boolean isTileThreatened(String Color,int x, int y){
@@ -1024,6 +1027,42 @@ public class GameBoard extends javax.swing.JFrame implements MouseListener ,Seri
         }
         return false;
         
+    }
+    
+    public static String checkCheckmate(ArrayList<Piece> AllPieceCheck){
+        int WhiteX=0;
+        int WhiteY=0;
+        int BlackX=0;
+        int BlackY=0;
+        for(int i=0;i<AllPieces.size();i++){
+            if(AllPieceCheck.get(i).color.equalsIgnoreCase("White") &&AllPieceCheck.get(i).pieceType.equalsIgnoreCase("King") && AllPieceCheck.get(i).alive)
+            {
+                 WhiteX = (int) AllPieceCheck.get(i).position.getX();
+                 WhiteY = (int) AllPieceCheck.get(i).position.getY();
+
+            }
+            else if(AllPieceCheck.get(i).color.equalsIgnoreCase("Black") &&AllPieceCheck.get(i).pieceType.equalsIgnoreCase("King") && AllPieceCheck.get(i).alive)
+            {
+                 BlackX = (int) AllPieceCheck.get(i).position.getX();
+                 BlackY = (int) AllPieceCheck.get(i).position.getY();
+
+            }
+        }
+        for(int i=0;i<AllPieces.size();i++){
+            if(AllPieceCheck.get(i).color.equalsIgnoreCase("Black") && AllPieceCheck.get(i).alive){
+                if(AllPieceCheck.get(i).validateMove(WhiteX, WhiteY))
+                {
+                    return "White";
+                }
+            }
+            else if(AllPieceCheck.get(i).color.equalsIgnoreCase("White") && AllPieceCheck.get(i).alive){
+                if(AllPieceCheck.get(i).validateMove(BlackX, BlackY))
+                {
+                    return "Black";
+                }
+            }
+        }
+        return null;
     }
 
 
